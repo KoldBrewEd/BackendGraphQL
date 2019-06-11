4f9ea4c29d4b958aad7d941e1acd4f2eac87af6d
Ed Lima <edlima@amazon.com>
2019-06-10 17:10:51 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Amplify, { graphqlOperation } from "aws-amplify";
 import { Connect } from "aws-amplify-react";
-
+import aws_exports from "./aws-exports";
 import * as queries from "./graphql/queries";
 import * as subscriptions from "./graphql/subscriptions";
+
+Amplify.configure(aws_exports);
 
 class App extends Component {
   render() {
@@ -14,29 +15,29 @@ class App extends Component {
         <h3>All Todos</h3>
         <ul>
           {todos.map(todo => (
-            <li key={todo.id}>
-              {todo.name} ({todo.id})
-            </li>
+            <p key={todo.id}>{todo.name}</p>
           ))}
         </ul>
       </div>
     );
 
     return (
-      <Connect
-        query={graphqlOperation(queries.listTodos)}
-        subscription={graphqlOperation(subscriptions.onCreateTodo)}
-        onSubscriptionMsg={(prev, { onCreateTodo }) => {
-          console.log(onCreateTodo);
-          return prev;
-        }}
-      >
-        {({ data: { listTodos }, loading, error }) => {
-          if (error) return <h3>Error</h3>;
-          if (loading || !listTodos) return <h3>Loading...</h3>;
-          return <ListView todos={listTodos ? listTodos.items : []} />;
-        }}
-      </Connect>
+      <div className="App">
+        <Connect
+          query={graphqlOperation(queries.listTodos)}
+          subscription={graphqlOperation(subscriptions.onCreateTodo)}
+          onSubscriptionMsg={(prev, { onCreateTodo }) => {
+            console.log("Subscription data:", onCreateTodo);
+            return prev;
+          }}
+        >
+          {({ data: { listTodos }, loading, error }) => {
+            if (error) return <h3>Error</h3>;
+            if (loading || !listTodos) return <h3>Loading...</h3>;
+            return <ListView todos={listTodos ? listTodos.items : []} />;
+          }}
+        </Connect>
+      </div>
     );
   }
 }
